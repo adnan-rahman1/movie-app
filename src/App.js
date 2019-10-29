7865fa05d3781d90b64f2a85e989ad671fc00ed2
Adnan Rahman <adnanibnamotin@yahoo.com>
2019-10-28 20:49:15 -0400

    create movie app using react app where you can type movie name and get movie details from open movie database api

--- src/App.js
+++ src/App.js
@@ -4,33 +4,58 @@ import './App.css';
 
 // User Defined Components
 import Header from './components/header/Header';
+import InputForm from './components/inputform/InputForm';
+import MovieDetails from './components/movie-details/MovieDetails';
 
-const api_key = "79a8cfa0bfe2f572624a45b859b24485";
+const api_key = "ab8eae77";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-     
+      title: "",
+      year: "",
+      released: "",
+      country: "",
+      genre: "",
+      director: "",
+      actors: "",
+      rating: "",
+      poster: "",
+      error: "Please enter a valid movie name"
     }
   }
 
   getMovie = async (e) => {
     
     const movie_name = e.target.elements.movie_name.value;
-
     e.preventDefault();
     
-    const api_call = await fetch(
-    );
+    const api_call = await fetch( `http://www.omdbapi.com/?t=${movie_name}&apikey=${api_key}`);
     const response = await api_call.json();
+
+    console.log(response);
+    this.setState({
+      title: response.Title,
+      year: response.Year,
+      country: response.Country,
+      released: response.Released,
+      genre: response.Genre,
+      director: response.Director,
+      actors: response.Actors,
+      rating: response.imdbRating,
+      poster: response.Poster,
+      error: "Please enter a valid movie name"
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Header />
+        <InputForm getMovie={this.getMovie}/>
+        <MovieDetails {...this.state}/>
       </div>
     )
   }
